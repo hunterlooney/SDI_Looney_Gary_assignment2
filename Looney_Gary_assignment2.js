0bfc1a917a50179e4eda7ea05278087c0742aed3
Gary W. H. Looney <hunterlooney@fullsail.edu>
2013-03-14 16:19:49 -0400

    Loops Practice

--- Looney_Gary_assignment2.js
+++ Looney_Gary_assignment2.js
@@ -1,1 +1,70 @@
-alert("JavaScript works!");
+// SDI Project 2
+// Gary W. Hunter Looney
+// Year: 13 Month: 03
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//loops
+/*
+//while loop
+var bOoj = 99 // counter
+
+while(bOoj > 0) {
+		
+		console.log(bOoj + " bottles of oj on the wall.")
+		bOoj--;		
+				
+				};
+
+console.log("No more bottles of OJ on the wall.");
+//for loop
+
+for (var booj = 99; booj > 0; booj--) {
+
+		console.log(booj + " bottles of oj on the wall.");
+		
+};
+
+console.log("No more bottles of OJ on the wall.");
+
+//for loop
+for (var i = 0; i < 5; i++) {
+		for (var ii = 0; ii < 3; ii++) {
+		console.log( "i: " + i + ", ii: " + ii);
+		};
+		
+};
+*/
